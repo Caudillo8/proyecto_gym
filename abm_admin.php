3aacd5bd4e575e470644f48f70939d3f5f00ad0b
hernandez matias <matias04hernandez5329@gmail.com>
2024-04-29 19:59:25 -0300

    ver modificar
    prueba ver datos y modificando solo el mail

--- abm_admin.php
+++ abm_admin.php
@@ -29,27 +29,45 @@
         </select>
         <input type="submit" name="boton_datos_clientes" value="VER">
         <br>
-    <?php
-        if(isset($_POST['boton_datos_clientes'])){
-            $id_clientes= $_POST['select_clientes'];  
-            $query_datos_clientes = "SELECT * from clientes WHERE id = '$id_clientes';";
-            $resultado_select_clientes = mysqli_query($conexion, $query_datos_clientes);
-            $dato_select_clientes = mysqli_fetch_assoc($resultado_select_clientes);
-            echo '<br>Número de socio :'.$id_clientes;
-            echo"<br>Apellido:\t".strtoupper($dato_select_clientes['apellido']);
-            echo"<br>Nombre :\t".strtoupper($dato_select_clientes['nombre']);
-            echo"<br> DNI :\t".strtoupper($dato_select_clientes['dni']);
-            echo"<br> Mail :\t".strtoupper($dato_select_clientes['mail']);
-            echo"<br> Teléfono :\t".strtoupper($dato_select_clientes['telefono']);
-            echo"<br> Sexo :\t".strtoupper($dato_select_clientes['sexo']);
-        }
-        
-
-    ?>
-
-
-
-    </form><!------------------------------------------------------------------------------>
-    
+    <br><label><strong> MODIFICAR EL CORREO</strong></label><BR>
+    <label>Ingrese nuevo mail</label>
+        <input type="text" name="cliente_nuevo_mail"><br>
+        <input type="submit" name="boton_modificar_mail_cliente" value="MODIFICAR">
+    </form><!--------------------------- form --------------------------------------------------->
 </body>
 </html>
+<!-------------------------- BOTON MOSTRAR DATOS CLIENTE--------------------------------------------->
+<?php
+    if(isset($_POST['boton_datos_clientes'])){
+        $id_clientes= $_POST['select_clientes'];  
+
+        $query_datos_clientes = "SELECT * from clientes WHERE id = '$id_clientes';";
+        $resultado_select_clientes = mysqli_query($conexion, $query_datos_clientes);
+        $dato_select_clientes = mysqli_fetch_assoc($resultado_select_clientes);
+        echo '<br>Número de socio :'.$id_clientes;
+        echo"<br>Apellido:\t".strtoupper($dato_select_clientes['apellido']);
+        echo"<br>Nombre :\t".strtoupper($dato_select_clientes['nombre']);
+        echo"<br> DNI :\t".strtoupper($dato_select_clientes['dni']);
+        echo"<br> Mail :\t".strtoupper($dato_select_clientes['mail']);
+        echo"<br> Teléfono :\t".strtoupper($dato_select_clientes['telefono']);
+        echo"<br> Sexo :\t".strtoupper($dato_select_clientes['sexo']);
+    }        
+//-------------------------- BOTON MODIFICAR CLIENTE---------------------------------------------//
+
+    if(isset($_POST['boton_modificar_mail_cliente'])){
+        $id_clientes= $_POST['select_clientes'];
+        $nuevo_mail_cliente = $_POST['cliente_nuevo_mail'];
+        $query_modificar_mail_cliente = "UPDATE clientes SET mail = '$nuevo_mail_cliente' WHERE id = '$id_clientes';";
+        $resultado_modificar_mail_cliente = mysqli_query($conexion, $query_modificar_mail_cliente);
+        if($resultado_modificar_mail_cliente){
+            echo"MODIFICA MAIL OK!";
+        }else{
+            echo"ERROR MODIFICAR MAIL CLIENTE";
+            exit;
+            }
+    }
+?>
+<!-------------------------- CIERRE CONEXION --------------------------------------------->
+<?php
+mysqli_close($conexion) ;
+?>
