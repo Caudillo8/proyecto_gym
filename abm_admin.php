72aeba9c41db5d0210e0ba2087a492f2a3a3e2c4
hernandez matias <matias04hernandez5329@gmail.com>
2024-05-01 20:43:55 -0300

    buscar , delete
    buscar por apellido, delete x id

--- abm_admin.php
+++ abm_admin.php
@@ -27,17 +27,23 @@
             </option>
             <?php } ?>
         </select>
-        <input type="submit" name="boton_datos_clientes" value="VER">
-        <br>
-    <br><label><strong> MODIFICAR EL CORREO</strong></label><BR>
-    <label>Ingrese nuevo mail</label>
+        <input type="submit" name="boton_datos_clientes" value="VER"><br>
+        <label><strong>Buscar por apellido</strong></label>
+        <input type="text" name="txt_buscar_cliente_nombre" placeholder="ingrese apellido">
+        <input type="submit" name="btn_buscar_apellido_cliente"value="BUSCAR">
+        <br><label><strong> Modificar correo del cliente seleccionado.</strong></label><BR>
+        <label>Ingrese nuevo mail</label>
         <input type="text" name="cliente_nuevo_mail"><br>
         <input type="submit" name="boton_modificar_mail_cliente" value="MODIFICAR">
+        <br><label><strong>Eliminar un cliente. </strong></label><BR>
+        <input type="text" name="txt_eliminar_cliente_nombre" placeholder="ingrese ID">
+        <input type="submit" name="btn_eliminar_apellido_cliente"value="DELETE">
     </form><!--------------------------- form --------------------------------------------------->
 </body>
 </html>
+<!-------------------------- P  H   P  --------------------------------------------->
 <!-------------------------- BOTON MOSTRAR DATOS CLIENTE--------------------------------------------->
-<?php
+<?php//------------------------------------------------------------------------------------------------------<?php
     if(isset($_POST['boton_datos_clientes'])){
         $id_clientes= $_POST['select_clientes'];  
 
@@ -66,7 +72,35 @@
             exit;
             }
     }
-?>
+//-------------------------- BOTON BUSCAR X APELLIDO CLIENTE---------------------------------------------//
+    if(isset($_POST['btn_buscar_apellido_cliente'])){
+        $apellido_cliente = $_POST['txt_buscar_cliente_nombre'];
+        $query_buscar_apellido_cliente = "SELECT * FROM clientes WHERE apellido = '$apellido_cliente';";
+        $rtdo_buscar_apellido_cliente = mysqli_query($conexion,$query_buscar_apellido_cliente);
+        if(! $rtdo_buscar_apellido_cliente){
+            echo"APELLIDO INEXISTENTE";
+            exit;
+        }
+        $datos_apellido_clientes = mysqli_fetch_assoc($rtdo_buscar_apellido_cliente);
+        echo '<br>Número de socio :'.$datos_apellido_clientes['id'];
+        echo"<br>Apellido:\t".strtoupper($datos_apellido_clientes['apellido']);
+        echo"<br>Nombre :\t".strtoupper($datos_apellido_clientes['nombre']);
+        echo"<br> DNI :\t".strtoupper($datos_apellido_clientes['dni']);
+        echo"<br> Mail :\t".strtoupper($datos_apellido_clientes['mail']);
+        echo"<br> Teléfono :\t".strtoupper($datos_apellido_clientes['telefono']);
+        echo"<br> Sexo :\t".strtoupper($datos_apellido_clientes['sexo']);
+    }
+//-------------------------- BOTON ELIMINAR CLIENTE X ID ---------------------------------------------//
+    if(isset($_POST['btn_eliminar_apellido_cliente'])){
+        $id_cliente_eliminar = intval($_POST['txt_eliminar_cliente_nombre']);
+        $query_id_cliente_eliminar = "DELETE FROM clientes WHERE id = '$id_cliente_eliminar';";
+        $rtdo_eliminar_cliente = mysqli_query($conexion, $query_id_cliente_eliminar);
+        if(! $rtdo_eliminar_cliente){
+            echo"ERROR DELETE CLIENTE";
+            exit;
+        }
+    }
+?><!-----------------------------------------------------------------------------------------------------------------php?--->
 <!-------------------------- CIERRE CONEXION --------------------------------------------->
 <?php
 mysqli_close($conexion) ;
