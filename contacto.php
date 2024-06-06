8f795fa4823c9d866c2a8a360ce911586dec980c
Caroso2 <caroso.martin@gmail.com>
2024-06-06 13:00:36 -0300

    Contacto + Funcionalidad
    Página de contacto agregada.
    Con Funcionalidad.

---
+++ contacto.php
@@ -0,0 +1,164 @@
+<!-- conexión al Formulario -->
+
+<?php
+
+        $servidor = "localhost";
+        $usuario = "root";
+        $clave = "";
+        $baseDeDatos = "probando";
+
+        $enlace = mysqli_connect ($servidor, $usuario, $clave, $baseDeDatos)
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Formulario</title>
+    <link rel="stylesheet" href="styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+  </head>
+  <body>
+    <header>
+        <nav class="navbar navbar-expand-lg bg-black">
+            <div class="container-fluid">
+              <a class="navbar-brand text-white" href="#">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-arms-up" viewBox="0 0 16 16">
+                    <path d="M8 3a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3"/>
+                    <path d="m5.93 6.704-.846 8.451a.768.768 0 0 0 1.523.203l.81-4.865a.59.59 0 0 1 1.165 0l.81 4.865a.768.768 0 0 0 1.523-.203l-.845-8.451A1.5 1.5 0 0 1 10.5 5.5L13 2.284a.796.796 0 0 0-1.239-.998L9.634 3.84a.7.7 0 0 1-.33.235c-.23.074-.665.176-1.304.176-.64 0-1.074-.102-1.305-.176a.7.7 0 0 1-.329-.235L4.239 1.286a.796.796 0 0 0-1.24.998l2.5 3.216c.317.316.475.758.43 1.204Z"/>
+                </svg>
+                NombreSistema
+              </a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon navbar-dark"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link text-white" href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar-week" viewBox="0 0 16 16">
+                            <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm-5 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z"/>
+                            <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5M1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4z"/>
+                        </svg>
+                        Agenda
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link text-white" href="#">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-file-person" viewBox="0 0 16 16">
+                            <path d="M12 1a1 1 0 0 1 1 1v10.755S12 11 8 11s-5 1.755-5 1.755V2a1 1 0 0 1 1-1zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+                            <path d="M8 10a3 3 0 1 0 0-6 3 3 0 0 0 0 6"/>
+                        </svg>
+                        Entrenadores
+                    </a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link text-white" href="contacto.php">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
+                        </svg>
+                        Contacto
+                      </a>
+                  </li>
+                </ul>
+                <div class="d-flex">
+                    <ul class="nav navbar-nav navbar-right">
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                                    <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6" />
+                                </svg>
+                                Ingresar
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link text-white" href="#">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0z" />
+                                    <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z" />
+                                </svg>
+                                Registrarse
+                            </a>
+                        </li>
+                    </ul>
+                  </div>
+              </div>
+            </div>
+          </nav>
+    </header>
+
+    <div class="container-fluid d-flex justify-content-center align-items-center" style="min-height: 670px; background-color: #164773;">
+        <div class="row align-items-center mx-auto">
+            <div class="col-lg-6">
+                <div class="card p-3 my-3 mx-auto" style="max-width: 1000px; background-color:black">
+                    <div class="card-body">
+                      <h1 class="card-title text-white">Contacto</h1>
+                      <p class="card-text text-white">¿Tenés alguna pregunta? ¡Acá estamos para ayudarte! Completa el formulario a continuación y nos vamos a poner en contacto con vos en la brevedad.</p>
+                    
+                    <!-- Formulario -->
+                    <form action="#" name="probando" method="post">
+                        <div class="mb-3">
+                            <label for="nombre" class="form-label" style="color: white;">Nombre</label>
+                            <input type="text" name="nombre" class="form-control" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="correo" class="form-label" style="color: white;">Correo electrónico</label>
+                            <input type="email" name="correo" class="form-control" required>
+                        </div>
+                        <div class="mb-3">
+                            <label for="telefono" class="form-label" style="color: white;">Teléfono</label>
+                            <input type="text" name="telefono" class="form-control" required>
+                        </div>
+                        <div class="d-grid gap-2">
+                            <button type="submit" name="registro" class="btn btn-secondary">Enviar</button>
+                            <button type="reset" class="btn btn-secondary">Limpiar</button>
+                        </div>
+                    </form>
+
+                    </div>
+                </div>
+                    <div class="col-lg-4 d-flex justify-content-center align-items-center">
+                       <img src="images/imagen-boxeadora.jpg" alt="Imagen de contacto" class="img-fluid rounded float-end" style="max-width: 600px; margin-right: -1500px; margin-top: -600px; border: 2px solid black;">
+                    </div>
+            </div>
+        </div>
+    </div>
+    <footer>
+        <div class="bg-black p-1">
+            <hr class="border border-white border-1 opacity-30 mx-auto w-50" />
+            <h5 class="text-center text-white my-3">© Aplicación web creada por Grupo 2, Tecnicatura Universitaria en Programación, UTN FRH.</h5>
+            <h5 class="text-center text-white mb-1">Integrantes del grupo:</h5>
+            <h6 class="text-center text-white mb-1">Carossino Martín</h6>
+            <h6 class="text-center text-white mb-1">Hernandez Matías</h6>
+            <h6 class="text-center text-white mb-1">Sarnari Valentín</h6>
+            <h6 class="text-center text-white mb-1">Yedro Fernando</h6>
+        </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  </body>
+
+<!-- Funcion del Formulario -->
+
+<?php
+
+if(isset($_POST['registro'])){
+    $nombre= $_POST ['nombre'];
+    $correo= $_POST ['correo'];
+    $telefono= $_POST ['telefono'];
+
+    $insertarDatos = "INSERT INTO datos VALUES ('$nombre', '$correo', '$telefono', '')";
+    $ejecutarInsertar = mysqli_query ($enlace, $insertarDatos);
+
+    // Verificar si la consulta se ejecutó correctamente
+    if ($ejecutarInsertar) {
+        echo '<script>alert("Mail enviado correctamente");</script>';
+    } else {
+        echo '<script>alert("Error al enviar el correo");</script>';
+    }
+}
+
+?>
+
+</html>
